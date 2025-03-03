9f2b8516061e239899a48b82450d126e05b9cad7
Tewos-21 <tewodrosshimels54@gmail.com>
2025-03-02 21:36:18 -0800

    add all changes

--- Back-end/server.js
+++ Back-end/server.js
@@ -4,6 +4,7 @@ import cors from "cors"
 // app config
 const app = express()
 const port = 4000
+
 // middleware
 app.use(express.json())
 app.use(cors())
@@ -15,3 +16,5 @@ app.get("/",(req,res) => {
 app.listen(port, () => {
     console.log('server starting on http://localhost:${port}')
 })
+
+// paused and continue from db creation and 3:45:27
